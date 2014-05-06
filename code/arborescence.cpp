12a68fa2894d4f5a377ab5f9b0b97de2cd3781ca
foreverbell <dql.foreverbell@gmail.com>
2014-05-06 23:51:54 +0800

    arborescence.cpp updated.

--- code/arborescence.cpp
+++ code/arborescence.cpp
@@ -2,13 +2,12 @@
 struct edge_t {
 	int u, v, w;
 } edge[maxm];
-
 void add(int u, int v, int w) {
 	edge[++ec].u = u, edge[ec].v = v, edge[ec].w = w;
 }
 
-int dirMST(int n, int root) {
-	int res = 0, ind;
+int arborescence(int n, int root) {
+	int res = 0, index;
 	while (true) {
 		for (int i = 1; i <= n; ++i) {
 			in[i] = maxint, vis[i] = -1, ID[i] = -1;
@@ -23,7 +22,7 @@ int dirMST(int n, int root) {
 			res += in[i];
 			if (in[i] == maxint) return -1;
 		}
-		ind = 0;
+		index = 0;
 		for (int i = 1; i <= n; ++i) {
 			if (vis[i] != -1) continue;
 			int u = i, v;
@@ -32,17 +31,17 @@ int dirMST(int n, int root) {
 				u = pre[u];
 			}
 			if (vis[u] != i || u == root) continue;
-			for (v = u, u = pre[u], ++ind; u != v; u = pre[u]) ID[u] = ind;
-			ID[v] = ind;
+			for (v = u, u = pre[u], ++index; u != v; u = pre[u]) ID[u] = index;
+			ID[v] = index;
 		}
-		if (ind == 0) return res;
-		for (int i = 1; i <= n; ++i) if (ID[i] == -1) ID[i] = ++ind;
+		if (index == 0) return res;
+		for (int i = 1; i <= n; ++i) if (ID[i] == -1) ID[i] = ++index;
 		for (int i = 1; i <= ec; ++i) {
 			int u = edge[i].u, v = edge[i].v;
 			edge[i].u = ID[u], edge[i].v = ID[v];
 			edge[i].w -= in[v];
 		}
-		n = ind, root = ID[root];
+		n = index, root = ID[root];
 	}
 	return res;
 }
