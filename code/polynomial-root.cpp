49f87a25e3355ee7a22cd343b30fa4a32466e822
foreverbell <dql.foreverbell@gmail.com>
2015-03-01 11:38:14 +0800

    fix polynomial root

--- code/polynomial-root.cpp
+++ code/polynomial-root.cpp
@@ -4,11 +4,10 @@ double cal(const vector<double> &coef, double x) {
 	return s;
 }
 
-double find(const vector<double> &coef, double l, double r) {
+double find(const vector<double> &coef, double l, double r, int sl, int sr) {
 	int sl = dblcmp(cal(coef, l)), sr = dblcmp(cal(coef, r));
 	if (sl == 0) return l;
 	if (sr == 0) return r;
-	if (sl * sr > 0) return maxdbl;
 	for (int tt = 0; tt < 100 && r - l > eps; ++tt) {
 		double mid = (l + r) / 2;
 		int smid = dblcmp(cal(coef, mid));
@@ -19,20 +18,30 @@ double find(const vector<double> &coef, double l, double r) {
 	return (l + r) / 2;
 }
 
-vector<double> solve(vector<double> coef, int n) {
-	vector<double> ret; // c[0]+c[1]*x+c[2]*x^2+...+c[n]*x^n
+vector<double> rec(const vector<double> &coef, int n) {
+	vector<double> ret; // c[0]+c[1]*x+c[2]*x^2+...+c[n]*x^n, c[n]==1
 	if (n == 1) {
-		if (dblcmp(coef[1]) != 0) ret.push_back(-coef[0] / coef[1]);
+		ret.push_back(-coef[0]);
 		return ret;
 	}
 	vector<double> dcoef(n);
-	for (int i = 0; i < n; ++i) dcoef[i] = coef[i + 1] * (i + 1);
-	vector<double> droot = solve(dcoef, n - 1);
-	droot.insert(droot.begin(), -maxdbl);
-	droot.push_back(maxdbl);
+	for (int i = 0; i < n; ++i) dcoef[i] = coef[i + 1] * (i + 1) / n;
+	double b = 2; // fujiwara bound
+	for (int i = 0; i <= n; ++i) b = max(b, 2 * pow(fabs(coef[i]), 1.0 / (n - i)));
+	vector<double> droot = rec(dcoef, n - 1);
+	droot.insert(droot.begin(), -b);
+	droot.push_back(b);
 	for (int i = 0; i + 1 < droot.size(); ++i) {
-		double tmp = find(coef, droot[i], droot[i + 1]);
-		if (tmp < maxdbl) ret.push_back(tmp);
+		int sl = dblcmp(cal(coef, droot[i])), sr = dblcmp(cal(coef, droot[i + 1]));
+		if (sl * sr > 0) continue;
+		ret.push_back(find(coef, droot[i], droot[i + 1], sl, sr));
 	}
 	return ret;
+}
+
+vector<double> solve(vector<double> coef) {
+	int n = coef.size() - 1;
+	while (coef.back() == 0) coef.pop_back(), --n;
+	for (int i = 0; i <= n; ++i) coef[i] /= coef[n];
+	return rec(coef, n);
 }
