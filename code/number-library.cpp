a2ef00c5e5bb4736acc79789e596f4a7464583e8
foreverbell <dql.foreverbell@gmail.com>
2016-02-19 10:23:00 +0800

    Tonelli–Shanks algorithm

--- code/number-library.cpp
+++ code/number-library.cpp
@@ -11,7 +11,7 @@ LL mult64(LL a, LL b, LL m) { // 64bit multiply 64bit
 LL fpow(LL a, LL p, int mod) { // fast power-modulo algorithm
 	LL res = 1;
 	for (; p; p >>= 1) {
-		if (p & 1) res = (res * a) % mod; // using mult64 when mod is 64-bit
+		if (p & 1) res = (res * a) % mod; // use mult64 when mod is 64-bit
 		a = (a * a) % mod;
 	}
 	return res;
@@ -75,7 +75,7 @@ bool miller_rabin(LL n, LL b) { // miller-rabin prime test
 	if (ret == 1 || ret == n - 1) return true;
 	--cnt;
 	while (cnt >= 0) {
-		ret = mult(ret, ret, n); // mult64
+		ret = mult64(ret, ret, n);
 		if (ret == n - 1) return true;
 		--cnt;
 	}
@@ -97,7 +97,7 @@ LL pollard_rho(LL n, LL seed) { // pollard-rho divisors factorization
 	x = y = rand() % (n - 1) + 1;
 	LL head = 1, tail = 2;
 	while (true) {
-		x = mult(x, x, n); // mult64
+		x = mult64(x, x, n); 
 		x = (x + seed) % n;
 		if (x == y) return n;
 		LL d = gcd(max(x - y, y - x), n);
@@ -168,4 +168,36 @@ int primtive_root(int p) {
 		}
 		if (flag) return root;
 	}
+}
+
+int legendre(int n, int p) {
+  return fpow(n, (p - 1) / 2, p);
+}
+
+// a.k.a Tonelli–Shanks algorithm
+int ressol(int n, int p) {
+  n %= p;
+  if (legendre(n, p) != 1) return -1;
+  int q = p - 1, s = 0, z, c;
+  while (q % 2 == 0) q /= 2, ++s;
+  while (true) {
+    z = rand() % (p - 1) + 1;
+    if (legendre(z, p) == p - 1) break; 
+  }
+  c = fpow(z, q, p);
+  int r = fpow(n, (q + 1) / 2, p), t = fpow(n, q, p), m = s;
+  while (true) {
+    if (t == 1) return r;
+    for (int i = 0, tmp = t; i < m; ++i, tmp = 1ll * tmp * tmp % p) {
+      if (tmp == 1) {
+        int b = fpow(c, 1 << (m - i - 1), p);
+        c = 1ll * b * b % p;
+        r = 1ll * r * b % p;
+        t = 1ll * t * c % p;
+        m = i;
+        break;
+      }
+    }
+  }
+  return r;
 }
